1953f90b8dec911a7b10c7c15bdcbf98e38423a7
Mohammad Junaid Ahmed <ahmedjunaid2004@gmail.com>
2025-10-25 20:50:15 +0530

    Initialized ContentCreationService

---
+++ src/main/java/com/ccs/ContentCreationService/ContentCreationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ccs.ContentCreationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCreationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCreationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccs/ContentCreationService/ContentCreationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccs.ContentCreationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCreationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
